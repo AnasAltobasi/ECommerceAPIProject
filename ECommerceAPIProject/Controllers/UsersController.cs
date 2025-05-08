b03477dea35ad6fe690f32e1d9163ccdbdf9bf59
Anas Tobasi <anas.tobasi@keitmea.com>
2025-05-08 14:10:38 +0300

    Get User Api

---
+++ ECommerceAPIProject/Controllers/UsersController.cs
@@ -0,0 +1,22 @@
+﻿using ECommerceAPIProject.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ECommerceAPIProject.Controllers
+{
+    [Authorize(Roles = "Admin")]
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UsersController : ControllerBase
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public UsersController(UserManager<ApplicationUser> userManager)
+            => _userManager = userManager;
+
+        [HttpGet]
+        public IActionResult GetUsers()
+            => Ok(_userManager.Users.ToList());
+    }
+}
